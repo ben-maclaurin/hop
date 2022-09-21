93e3cb2132e51d934cdcbc83e221c2ad7cf157c6
Ben <ben@thedesigncollaborative.co>
2022-09-21 16:10:03 +0100

    feat: rename project to hop

--- src/configuration.rs
+++ src/configuration.rs
@@ -24,7 +24,7 @@ impl Default for Configuration {
         Self {
             directory: String::from(""),
             editor: Editor::VSCode,
-            title: String::from("Project directories"),
+            title: String::from("Hop"),
         }
     }
 }
